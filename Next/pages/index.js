280e7602d4fcb3e8954f02814b4eaf62201d0574
Gabrielygor <gabrielcanuto042@gmail.com>
2024-11-11 08:38:46 -0300

    feat: Add Next recipe 1

---
+++ Next/pages/index.js
@@ -0,0 +1,18 @@
+import { Card } from "./card"
+
+export default function Home() {
+
+    return (
+
+        <div>
+            <h1>
+                Next Recipes
+            </h1>
+
+            <Card/>
+
+        </div>
+
+    )
+
+}
