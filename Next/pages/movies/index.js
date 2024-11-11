9e616a8499fdcd7fd51552c86f039588bea245e1
Gabrielygor <gabrielcanuto042@gmail.com>
2024-11-11 11:15:49 -0300

    feat: Add Next recipe 3

---
+++ Next/pages/movies/index.js
@@ -0,0 +1,51 @@
+import { useState } from "react"
+
+export default function Movies({ initialData }) {
+    const [searchTerm, setSearchTerm] = useState("")
+    const [data, setData] = useState(initialData)
+
+    async function searchMovies() {
+        const res = await fetch(
+            `http://www.omdbapi.com/?apikey=ca98445&s=${searchTerm}`
+        )
+        const searchData = await res.json()
+        setData(searchData)
+    }
+
+    return (
+        <div className="bg-zinc-700 flex flex-col justify-center items-center">
+            <div className="flex justify-center gap-3 items-center bg-zinc-500 w-full h-12 ">
+                <input
+                    type="text"
+                    placeholder="Digite a palavra-chave"
+                    className="bg-black"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                />
+                <button className="bg-white text-black font-bold p-1 rounded" onClick={searchMovies}>Pesquisar</button>{" "}
+            </div>
+            <div className="grid grid-cols-2 gap-3 mt-5">
+                {data &&
+                    data.Search &&
+                    data.Search.map((m) => (
+                        <div key={m.imdbID} className="bg-zinc-900 flex flex-col justify-center items-center p-6 gap-5 rounded-2xl transition-all hover:bg-black">
+                            <h2 className="text-lime-500">
+                                {m.Title} --- {m.Year} --- {m.Type}
+                            </h2>
+                            <img src={m.Poster} width="180px" alt={m.Title} />
+                        </div>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export async function getServerSideProps(context) {
+    const res = await fetch(`http://www.omdbapi.com/?apikey=ca98445&s=bagdad`)
+    const initialData = await res.json()
+    return {
+        props: {
+            initialData,
+        },
+    }
+}
