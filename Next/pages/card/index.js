7531ea653fad57f160a28238af0ebefdb6baefe8
Gabrielygor <gabrielcanuto042@gmail.com>
2024-12-18 17:00:18 -0300

    refactor: remove next recipes

--- Next/pages/card/index.js
+++
@@ -1,9 +0,0 @@
-export function Card (props) {
-    return  (
-        <div className="text-white bg-zinc-800 p-3 h-11 flex justify-center items-center rounded-lg transition-all hover:bg-white hover:text-black hover:border hover:border-spacing-1 hover:border-black">
-            <h1 className="text-lg font-bold">
-                {props.card}
-            </h1>
-        </div>
-    )
-}
